2ab03ea162a527bdb8d38ce08b0beff7717988bd
Neema MV <mvneema10@gmail.com>
2020-08-18 14:04:39 +0530

    uploading data and data collection python script for glassdoor & indeed

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 00:49:50 2020
+
+@author: Neema MV
+"""
+
+import ds_proj_sal_pred as gs
+import pandas as pd
+path = "C:/Users/Neema MV/Documents/ds_proj/chromedriver"
+
+ds = gs.get_jobs('data scientist', 15, False, path, 15)
